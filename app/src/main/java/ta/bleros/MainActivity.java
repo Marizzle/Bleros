dd0f46ec6f815e3c3e2584f5af182242c5976e32
kevsgrove <kevinc@enlume.com>
2015-07-18 18:38:48 -0700

    scrapped original idea. Back to helloworld

--- app/src/main/java/ta/bleros/MainActivity.java
+++ app/src/main/java/ta/bleros/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, Board.class));
     }
 
     @Override
