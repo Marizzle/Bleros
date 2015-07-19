ff7cf53860d38f683eae92802d22e8e9f857f439
kevsgrove <kevinc@enlume.com>
2015-07-18 19:27:31 -0700

    Audio test

--- app/src/main/java/ta/bleros/MainActivity.java
+++ app/src/main/java/ta/bleros/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, AudioRecordTest.class));
     }
 
     @Override
